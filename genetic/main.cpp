8c191c6d8473898a507fbab0e357613596923364
Benjamin Saunders <besaunde@sfu.ca>
2011-06-24 19:11:04 -0700

    Initial skeleton of genetic approach.

---
+++ genetic/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  if(argc != 2) {
+    cerr << "Usage: " << argv[0] << " <dataset>" << endl;
+  }
+  
+  return 0;
+}
